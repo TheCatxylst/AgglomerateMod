3c7197e24c07cd399c9ab8b8a3ebd918be5a23e9
The Catxylst <deboned.cat@gmail.com>
2025-07-16 00:02:09 -0400

    sad

--- obj/Debug/net8.0/Agglomerate.AssemblyInfo.cs
+++ obj/Debug/net8.0/Agglomerate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agglomerate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ecba5a7b4523ff2520a246d7da654355b0489a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+119cd4252404d40626158d418e5b9996cc7a03af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agglomerate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agglomerate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
